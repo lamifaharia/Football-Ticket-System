5d72ca55dd216a38e853ff3ccf6cac99bc5201f1
lamifaharia <lamifaharia@gmail.com>
2026-06-16 20:05:43 +0600

    inserting the data

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Users VALUES (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111');
+INSERT INTO Users VALUES (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222');
+INSERT INTO Users VALUES (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333');
+INSERT INTO Users VALUES (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
